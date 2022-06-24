ce9c2805718701ad9c2068c411ac16e8e4945352
Gleb Dovzhenko <dovjenko.g@gmail.com>
2022-06-24 17:44:10 +0700

    Updated 1-5 params

--- params/params_1_5.py
+++ params/params_1_5.py
@@ -1,11 +1,19 @@
+import numpy as np
+
 front_end_distance = 15000  # from source
 front_end_h_angle = 2.0e-3  # rad
 front_end_v_angle = 0.2e-3  # rad
+front_end_opening = [
+    -front_end_distance * np.tan(front_end_h_angle / 2.),
+    front_end_distance * np.tan(front_end_h_angle / 2.),
+    -front_end_distance * np.tan(front_end_v_angle / 2.),
+    front_end_distance * np.tan(front_end_v_angle / 2.)
+]
 
 filter_distance = 18000  # from source
 
 monochromator_distance = 33500  # from source
 monochromator_z_offset = 25  # fixed beam offset in z direction
 
-monochromator_slit_distance = 115000  # from source
-monochromator_slit_opening = [-107.5, 107.5, -10, 10]
+exit_slit_distance = 115000  # from source
+exit_slit_opening = [-107.5, 107.5, -10., 10.]
