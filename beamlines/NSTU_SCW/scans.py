40d6be621043db1347b75264d2c82ba75ce274e4
Gleb Dovzhenko <dovjenko.g@gmail.com>
2023-10-05 08:36:43 +0700

    scan_mask_opening_90

--- beamlines/NSTU_SCW/scans.py
+++ beamlines/NSTU_SCW/scans.py
@@ -118,12 +118,12 @@ def onept(bl: NSTU_SCW, plts: List):
 
 def scan_mask_opening(bl: NSTU_SCW, plts: List):
     subdir = os.path.join(os.getenv('BASE_DIR', ''), 'datasets', 'nstu-scw-2')
-    scan_name = 'scan_mask_opening_70'
+    scan_name = 'scan_mask_opening_90'
 
     if not os.path.exists(os.path.join(subdir, scan_name)):
         os.mkdir(os.path.join(subdir, scan_name))
 
-    en = 70.e3
+    en = 90.e3
     if np.isclose(en, 30e3):
         r1, r2 = -2.04e3, -2.04e3  # 30 keV
         g_f = 1.076                # 30 keV
@@ -167,7 +167,7 @@ def scan_mask_opening(bl: NSTU_SCW, plts: List):
                 plot.persistentName = plot.saveName.replace('.png', '.pickle')
 
                 plot.xaxis.limits = [-.5, .5]
-                plot.yaxis.limits = [-.3, .3]
+                plot.yaxis.limits = [-.1, .1]
 
         yield
 
