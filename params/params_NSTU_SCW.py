39ca6fb8fa82688e65951a4c65b9972bc428c0ea
Gleb Dovzhenko <dovjenko.g@gmail.com>
2023-11-30 13:14:50 +0700

    Set glassy carbon as lens material [2]

--- params/params_NSTU_SCW.py
+++ params/params_NSTU_SCW.py
@@ -39,8 +39,8 @@ croc_crl_distance = 28000.  # from source
 # croc_crl_y_t = 0.58
 # croc_crl_L = 81
 # glassy carbon	
-croc_crl_y_t = 0.58
-croc_crl_L = 115.7
+croc_crl_y_t = 0.6
+croc_crl_L = 115.
 
 exit_slit_distance = 56000.  # from source
 
