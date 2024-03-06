a8298a5473e5d3705fe135bce325de8152114598
Gleb Dovzhenko <dovjenko.g@gmail.com>
2024-03-06 15:34:24 +0700

    removed deprecated CrocLens component

--- components/__init__.py
+++ components/__init__.py
@@ -2,5 +2,4 @@ from .BentLaueParaboloid import BentLaueParaboloid
 from .BentLaueCylinder import BentLaueCylinder
 from .CrystalSiPrecalc import CrystalSiPrecalc
 from .BentLaueParaboloidWithBump import BentLaueParaboloidWithBump
-from .CrocLens import CrocLens
 from .PrismaticLens import PrismaticLens
--- params/params_1_3.py
+++ params/params_1_3.py
@@ -3,3 +3,10 @@ front_end_h_angle = 1.8e-3  # rad
 front_end_v_angle = 0.1e-3  # rad
 
 filter_distance = 18000  # from source
+
+sample_1_distance = 45000
+sample_2_distance = 122000
+
+croc_1_distance = 37000
+croc_2_distance = 52000
+
