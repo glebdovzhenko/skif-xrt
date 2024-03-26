ed9e8aac81521b97cfeb77f851c58a01e2404e56
Gleb Dovzhenko <dovjenko.g@gmail.com>
2024-03-26 10:01:05 +0700

    have a working 1-3 with CRL stack

--- beamlines/SKIF_1_3/scans.py
+++ beamlines/SKIF_1_3/scans.py
@@ -2,7 +2,7 @@ from typing import List
 import xrt.plotter as xrtplot
 import xrt.runner as xrtrun
 
-from SKIF_1_3 import SKIF13 
+from SKIF_1_3 import SKIF13
 
 
 # ################################### PLOTS ###################################
@@ -10,35 +10,40 @@ from SKIF_1_3 import SKIF13
 
 plots = [
     xrtplot.XYCPlot(
-        beam='WgMonitorLocal',
-        title='Wiggler Monitor',
-        xaxis=xrtplot.XYCAxis(r'$x$', 'mm', limits=(0., 0.)),
-        yaxis=xrtplot.XYCAxis(r'$z$', 'mm', limits=(0., 0.)),
-        aspect='auto'),
+        beam="WgMonitorLocal",
+        title="Wiggler Monitor",
+        xaxis=xrtplot.XYCAxis(r"$x$", "mm", limits=(0.0, 0.0)),
+        yaxis=xrtplot.XYCAxis(r"$z$", "mm", limits=(0.0, 0.0)),
+        aspect="auto",
+    ),
     xrtplot.XYCPlot(
-        beam='FEMonitorLocal',
-        title='Front-End Monitor',
-        xaxis=xrtplot.XYCAxis(r'$x$', 'mm', limits=(0., 0.)),
-        yaxis=xrtplot.XYCAxis(r'$z$', 'mm', limits=(0., 0.)),
-        aspect='auto'),
+        beam="FEMonitorLocal",
+        title="Front-End Monitor",
+        xaxis=xrtplot.XYCAxis(r"$x$", "mm", limits=(0.0, 0.0)),
+        yaxis=xrtplot.XYCAxis(r"$z$", "mm", limits=(0.0, 0.0)),
+        aspect="auto",
+    ),
     xrtplot.XYCPlot(
-        beam='LensEntranceMonitorLocal',
-        title='Lens Entrance Monitor',
-        xaxis=xrtplot.XYCAxis(r'$x$', 'mm', limits=(0., 0.)),
-        yaxis=xrtplot.XYCAxis(r'$z$', 'mm', limits=(0., 0.)),
-        aspect='auto'),
+        beam="LensEntranceMonitorLocal",
+        title="Lens Entrance Monitor",
+        xaxis=xrtplot.XYCAxis(r"$x$", "mm", limits=(0.0, 0.0)),
+        yaxis=xrtplot.XYCAxis(r"$z$", "mm", limits=(0.0, 0.0)),
+        aspect="auto",
+    ),
     xrtplot.XYCPlot(
-        beam='LensExitMonitorLocal',
-        title='Lens Exit Monitor',
-        xaxis=xrtplot.XYCAxis(r'$x$', 'mm', limits=(0., 0.)),
-        yaxis=xrtplot.XYCAxis(r'$z$', 'mm', limits=(0., 0.)),
-        aspect='auto'),
+        beam="LensExitMonitorLocal",
+        title="Lens Exit Monitor",
+        xaxis=xrtplot.XYCAxis(r"$x$", "mm", limits=(0.0, 0.0)),
+        yaxis=xrtplot.XYCAxis(r"$z$", "mm", limits=(0.0, 0.0)),
+        aspect="auto",
+    ),
     xrtplot.XYCPlot(
-        beam='SampleMonitorLocal',
-        title='Sample Monitor',
-        xaxis=xrtplot.XYCAxis(r'$x$', 'mm', limits=(0., 0.)),
-        yaxis=xrtplot.XYCAxis(r'$z$', 'mm', limits=(0., 0.)),
-        aspect='auto'),
+        beam="SampleMonitorLocal",
+        title="Sample Monitor",
+        xaxis=xrtplot.XYCAxis(r"$x$", "mm", limits=(0.0, 0.0)),
+        yaxis=xrtplot.XYCAxis(r"$z$", "mm", limits=(0.0, 0.0)),
+        aspect="auto",
+    ),
 ]
 
 
@@ -48,30 +53,61 @@ plots = [
 def none_scan(plts: List[xrtplot.XYCPlot], bl: SKIF13):
     # setting up axes limits
     for plt in plts:
-        if plt.beam in ('WgMonitorLocal', 'FEMonitorLocal'):
-            plt.xaxis.limits = 1.4 * bl.FrontEnd.opening[0], 1.4 * bl.FrontEnd.opening[1]
-            plt.yaxis.limits = 1.4 * bl.FrontEnd.opening[2], 1.4 * bl.FrontEnd.opening[3]
-        if plt.beam in ('LensEntranceMonitorLocal', 'LensExitMonitorLocal'):
-            plt.xaxis.limits = 1.4 * bl.CrocLensStack[0].limPhysX[0], 1.4 * bl.CrocLensStack[0].limPhysX[1]
-            plt.yaxis.limits = 1.4 * bl.CrocLensStack[0].limPhysY[0], 1.4 * bl.CrocLensStack[0].limPhysY[1]
-            if plt.beam == 'LensExitMonitorLocal':
-                plt.yaxis.limits = -.7 * bl.lens_pars['Aperture'], .7 * bl.lens_pars['Aperture']
-        if plt.beam == 'SampleMonitorLocal':
-            plt.xaxis.limits = 1.4 * bl.SampleSlit.opening[0], 1.4 * bl.SampleSlit.opening[1]
-            plt.yaxis.limits = -.7 * bl.lens_pars['Aperture'], .7 * bl.lens_pars['Aperture']
-    
+        if plt.beam in ("WgMonitorLocal", "FEMonitorLocal"):
+            plt.xaxis.limits = (
+                1.4 * bl.FrontEnd.opening[0],
+                1.4 * bl.FrontEnd.opening[1],
+            )
+            plt.yaxis.limits = (
+                1.4 * bl.FrontEnd.opening[2],
+                1.4 * bl.FrontEnd.opening[3],
+            )
+        if plt.beam in ("LensEntranceMonitorLocal", "LensExitMonitorLocal"):
+            plt.xaxis.limits = (
+                1.4 * bl.CrocLensStack[0].limPhysX[0],
+                1.4 * bl.CrocLensStack[0].limPhysX[1],
+            )
+            plt.yaxis.limits = (
+                1.4 * bl.CrocLensStack[0].limPhysY[0],
+                1.4 * bl.CrocLensStack[0].limPhysY[1],
+            )
+            if plt.beam == "LensExitMonitorLocal":
+                plt.yaxis.limits = (
+                    -0.7 * bl.lens_pars["Aperture"],
+                    0.7 * bl.lens_pars["Aperture"],
+                )
+        if plt.beam == "SampleMonitorLocal":
+            plt.xaxis.limits = (
+                1.4 * bl.SampleSlit.opening[0],
+                1.4 * bl.SampleSlit.opening[1],
+            )
+            plt.yaxis.limits = (
+                -0.7 * bl.lens_pars["Aperture"],
+                0.7 * bl.lens_pars["Aperture"],
+            )
+
     # setting up plot titles
     for plt in plts:
-        if plt.beam == 'WgMonitorLocal':
-            plt.title = ' '.join((plt.title, '[%.01f m]' % (bl.WigglerMonitor.center[1] * 1e-3)))
-        elif plt.beam == 'FEMonitorLocal':
-            plt.title = ' '.join((plt.title, '[%.01f m]' % (bl.FrontEndMonitor.center[1] * 1e-3)))
-        elif plt.beam == 'LensEntranceMonitorLocal':
-            plt.title = ' '.join((plt.title, '[%.01f m]' % (bl.LensEntranceMonitor.center[1] * 1e-3)))
-        elif plt.beam == 'LensExitMonitorLocal':
-            plt.title = ' '.join((plt.title, '[%.01f m]' % (bl.LensExitMonitor.center[1] * 1e-3)))
-        elif plt.beam == 'SampleMonitorLocal':
-            plt.title = ' '.join((plt.title, '[%.01f m]' % (bl.SampleMonitor.center[1] * 1e-3)))
+        if plt.beam == "WgMonitorLocal":
+            plt.title = " ".join(
+                (plt.title, "[%.01f m]" % (bl.WigglerMonitor.center[1] * 1e-3))
+            )
+        elif plt.beam == "FEMonitorLocal":
+            plt.title = " ".join(
+                (plt.title, "[%.01f m]" % (bl.FrontEndMonitor.center[1] * 1e-3))
+            )
+        elif plt.beam == "LensEntranceMonitorLocal":
+            plt.title = " ".join(
+                (plt.title, "[%.01f m]" % (bl.LensEntranceMonitor.center[1] * 1e-3))
+            )
+        elif plt.beam == "LensExitMonitorLocal":
+            plt.title = " ".join(
+                (plt.title, "[%.01f m]" % (bl.LensExitMonitor.center[1] * 1e-3))
+            )
+        elif plt.beam == "SampleMonitorLocal":
+            plt.title = " ".join(
+                (plt.title, "[%.01f m]" % (bl.SampleMonitor.center[1] * 1e-3))
+            )
 
     yield
 
@@ -79,11 +115,11 @@ def none_scan(plts: List[xrtplot.XYCPlot], bl: SKIF13):
 # ################################### MAIN ####################################
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     beamline = SKIF13()
     scan = none_scan
     show = False
-    repeats = 10
+    repeats = 1
 
     if show:
         beamline.glow(
@@ -91,7 +127,7 @@ if __name__ == '__main__':
             centerAt=2,
             generator=scan,
             generatorArgs=[plots, beamline],
-            startFrom=1
+            startFrom=1,
         )
     else:
         xrtrun.run_ray_tracing(
@@ -100,5 +136,5 @@ if __name__ == '__main__':
             repeats=repeats,
             backend=r"raycing",
             generator=scan,
-            generatorArgs=[plots, beamline]
+            generatorArgs=[plots, beamline],
         )
